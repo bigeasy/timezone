de2a2140d99eed6ada77d0db2922b4b955e8e2ff
Alan Gutierrez <alan@prettyrobots.com>
2012-05-08 21:56:46 -0400

    Working RFC 822 parser.
    An optimistic, yet picky, RFC 822 date parser. Matches and verifies both two
    digit and four digit years, but I'm not sure what `tz` will make of a two digit
    year. Probably nothing, since it simply calls `Date.UTC`.
    
    Code clean up only. Not yet modularized.
    
        File                             Raw         Min         Zip     Min/Zip
        rfc822.js                 1978/01.93  1659/01.62  1022/01.00   957/00.93

--- rfc822.js
+++ rfc822.js
@@ -9,49 +9,45 @@
 //
 // However, this is a no go right now. Not even going to open a ticket. Probably
 // going to shuffle this off to a branch and delete it from the main branch.
-    if (match = /^(.*?)(\w{3}),\s+(\d{1,2})\s+(\w{3})\s+(\d{2,4})\s+(\d{2}):(\d{2})(?::(\d{2}))?\s*(?:([A-IK-Z]|UT|GMT|[ECMP][SD]T)|([-+]?\d{4}))?(.*)$/i.exec(pattern)) {
-      _ref = match.slice(1), before = _ref[0], dow = _ref[1], day = _ref[2], month = _ref[3], year = _ref[4], hours = _ref[5], minutes = _ref[6], seconds = _ref[7], zone = _ref[8], offset = _ref[9], after = _ref[10];
-      dow = dow.toLowerCase();
-      _ref1 = request.locales[request.locale].day.abbrev;
-      for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-        abbrev = _ref1[_i];
-        if (dow === abbrev.toLowerCase()) {
-          dow = null;
-          break;
-        }
-      }
-      if (dow) {
-        throw new Error("bad weekday");
-      }
-      month = month.toLowerCase();
-      _ref2 = request.locales[request.locale].month.abbrev;
-      for (i = _j = 0, _len1 = _ref2.length; _j < _len1; i = ++_j) {
-        abbrev = _ref2[i];
-        if (month === abbrev.toLowerCase()) {
-          month = i;
-          break;
-        }
-      }
-      if (typeof month === "string") {
-        throw new Error("bad month");
-      }
-      seconds || (seconds = "0");
-      offset || (offset = "0");
-      _ref3 = (function() {
-        var _k, _len2, _ref3, _results;
-        _ref3 = [day, year, hours, minutes, seconds, offset];
-        _results = [];
-        for (_k = 0, _len2 = _ref3.length; _k < _len2; _k++) {
-          num = _ref3[_k];
-          _results.push(parseInt(num, 10));
-        }
-        return _results;
-      })(), day = _ref3[0], year = _ref3[1], hours = _ref3[2], minutes = _ref3[3], seconds = _ref3[4], offset = _ref3[5];
-      wallclock = makeDate(year, month + 1, day, hours, minutes, seconds, 0);
-      if (offset) {
-        posix = wallclock - Math.floor(offset / 100) * HOUR;
-      } else {
-        posix = convertToPOSIX(request, wallclock);
-      }
-      return posix;
+/*
+var __slice = [].slice;
+
+function die () {
+  console.log.apply(console, __slice.call(arguments, 0));
+  return process.exit(1);
+};
+
+function say () { return console.log.apply(console, __slice.call(arguments, 0)) }
+*/
+
+function rfc822(pattern) {
+    var day
+      , date
+      , match = /^\s*(\w{3}),\s+(\d{1,2})\s+(\w{3})\s+(\d{2,4})\s+(\d{2}):(\d{2})(?::(\d{2}))?\s*(?:([-+]?\d{4})|([A-IK-Z]|UT|GMT|[ECMP][SD]T))?\s*$/i.exec(pattern);
+    if (!match) throw Error("invalid rfc822 date");
+
+    match[1] = "sun|mon|tue|wed|thu|fri|sat".split("|").indexOf(match[1].toLowerCase());
+    match[3] = "jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec".split("|").indexOf(match[3].toLowerCase());
+    for (i = 1; i < 9; i++) {
+      match[i] = parseInt(match[i] || "0", 10);
     }
+    match[0] = match[4];
+    match[4] = match[2];
+    match[2] = match[0];
+    
+    day = match[1];
+    offset = match[9] || match[8];
+    match = match.slice(2, 8)
+
+    date = new Date(Date.UTC.apply(Date.UTC, match));
+
+    if (date.getUTCDay() != day) throw new Error("invalid rfc822 date");
+
+    if (offset) match.push(offset < 0 ? "-" : "+", Math.abs(Math.floor(offset / 100)), offset % 100);
+    else if (offset == "GMT") match.push("+", 0);
+
+    return match;
+}
+
+
+rfc822("Fri, 08 May 92 21:38:46 -0400");
