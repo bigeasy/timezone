ef5764088c37fea20dad50bbe23ffd365676cf1d
Alan Gutierrez <alan@prettyrobots.com>
2012-06-26 10:34:51 -0400

    Run profile longer.
    Try to get the profile to run for at least ten seconds.

--- utility/profile-tz.js
+++ utility/profile-tz.js
@@ -3,5 +3,5 @@ var tz = require("timezone/index");
 
 (function () {
   var d = Date.now();
-  for (var i = 0; i < 100000; i++) tz(d, "%m-%d-%Y %I:%M:%S %P");
+  for (var i = 0; i < 1000000; i++) tz(d, "%m-%d-%Y %I:%M:%S %P");
 })();
