35c21129d8d271e0237972dfe72c9d32f3ed0448
Alan Gutierrez <alan@prettyrobots.com>
2012-06-26 20:47:06 -0400

    Format with switch.

--- util/profile-tz.js
+++ util/profile-tz.js
@@ -3,6 +3,6 @@ var tz = require("timezone/index");
 
 (function () {
   var d = Date.now();
-  //console.log(tz(d, "%m-%d-%Y %I:%M:%S %P"));
-   for (var i = 0; i < 1000000; i++) tz(d, "%m-%d-%Y %I:%M:%S %P");
+ // console.log(tz(d, "%m-%d-%Y %I:%M:%S %P"));
+  for (var i = 0; i < 1000000; i++) tz(d, "%m-%d-%Y %I:%M:%S %P");
 })();
