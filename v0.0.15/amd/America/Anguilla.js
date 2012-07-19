ffe05b81e2c8775412d8033585ff0ffa1c4f09cf
Alan Gutierrez <alan@prettyrobots.com>
2012-07-18 23:09:15 -0400

    Release 0.0.15 data.

---
+++ v0.0.15/amd/America/Anguilla.js
@@ -0,0 +1,1 @@
+define(function () { return {"zones":{"America/Anguilla":["z",{"wallclock":-1825113600000,"format":"AST","abbrev":"AST","offset":-14400000,"posix":-1825098464000,"save":0},{"wallclock":-1.7976931348623157e+308,"format":"LMT","abbrev":"LMT","offset":-15136000,"posix":-1.7976931348623157e+308,"save":0}]},"rules":{}} });
---
+++ v0.0.15/json/WET.js
@@ -0,0 +1,16 @@
+{
+  "zones": {
+    "WET": [
+      "z",
+      {
+        "wallclock": -1.7976931348623157e+308,
+        "format": "WE%sT",
+        "abbrev": "WE%sT",
+        "offset": 0,
+        "posix": -1.7976931348623157e+308,
+        "save": 0
+      }
+    ]
+  },
+  "rules": {}
+}
