ccceab00b4e9f8f23126de15c6c9305f6b337c44
Alan Gutierrez <alan@prettyrobots.com>
2013-03-13 20:17:54 -0400

    Data files for v0.0.19.

---
+++ v0.0.19/jsonp/America/Montserrat.js
@@ -0,0 +1,1 @@
+timezoneJSONPCallback({"zones":{"America/Montserrat":["z",{"wallclock":-1846281540000,"format":"AST","abbrev":"AST","offset":-14400000,"posix":-1846266608000,"save":0},{"wallclock":-1.7976931348623157e+308,"format":"LMT","abbrev":"LMT","offset":-14932000,"posix":-1.7976931348623157e+308,"save":0}]},"rules":{}});
---
+++ v0.0.19/jsonp/Pacific/Johnston.js
@@ -0,0 +1,1 @@
+timezoneJSONPCallback({"zones":{"Pacific/Johnston":["z",{"wallclock":-1.7976931348623157e+308,"format":"HST","abbrev":"HST","offset":-36000000,"posix":-1.7976931348623157e+308,"save":0}]},"rules":{}});
