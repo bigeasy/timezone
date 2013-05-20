a3b0edf359acd43905bff0fc11a79b58f7ed6d87
Alan Gutierrez <alan@prettyrobots.com>
2013-05-20 12:55:26 -0400

    Data files for v0.0.21.
    See #159.

---
+++ v0.0.21/jsonp/America/Tortola.js
@@ -0,0 +1,1 @@
+timezoneJSONPCallback({"zones":{"America/Tortola":["z",{"wallclock":-1846281600000,"format":"AST","abbrev":"AST","offset":-14400000,"posix":-1846266092000,"save":0},{"wallclock":-1.7976931348623157e+308,"format":"LMT","abbrev":"LMT","offset":-15508000,"posix":-1.7976931348623157e+308,"save":0}]},"rules":{}});
