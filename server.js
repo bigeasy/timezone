79c427da9b742e5cb4b3150ff87bc728959de780
Alan Gutierrez <alan@prettyrobots.com>
2013-07-24 10:39:20 -0400

    Remove old server.
    See #175.

--- server.js
+++
@@ -1,6 +0,0 @@
-var connect = require("connect");
-
-var app = connect()
-  .use(connect.logger())
-  .use(connect.static(process.argv[2]))
-  .listen(8086);
--- server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-dir=$(basename "$DIR")
-
-(cd .. && node "$dir/server.js" "$dir")
