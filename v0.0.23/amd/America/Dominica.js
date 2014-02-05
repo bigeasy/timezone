1698f024811cfb4518a5c45f8aace9111a51708c
Alan Gutierrez <alan@prettyrobots.com>
2014-02-05 08:48:33 +0000

    Data files for v0.0.23.
    See #170.

---
+++ v0.0.23/amd/America/Dominica.js
@@ -0,0 +1,1 @@
+define(function () { return {"zones":{"America/Dominica":["z",{"wallclock":-1846281540000,"format":"AST","abbrev":"AST","offset":-14400000,"posix":-1846266804000,"save":0},{"wallclock":-1.7976931348623157e+308,"format":"LMT","abbrev":"LMT","offset":-14736000,"posix":-1.7976931348623157e+308,"save":0}]},"rules":{}} });
