a5576ab27b3eecd93ed5035a8271651522ddbfac
Alan Gutierrez <alan@prettyrobots.com>
2014-08-07 01:55:45 +0000

    Release 0.0.34.
    Closes #216.

--- src/timezone.js
+++ src/timezone.js
@@ -109,7 +109,7 @@
   }
 
   function convert (vargs) {
-    if (!vargs.length) return "0.0.33";
+    if (!vargs.length) return "0.0.34";
 
     var request = Object.create(this)
       , adjustments = []
--- t/version/get.t.js
+++ t/version/get.t.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 require("../proof")(1, function (equal, tz) {
-  equal(tz(), "0.0.33", "get");
+  equal(tz(), "0.0.34", "get");
 });
