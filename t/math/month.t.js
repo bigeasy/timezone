1fd4e417143edeb488d256be8c09d0fdb1d6b84b
Alan Gutierrez <alan@prettyrobots.com>
2015-09-20 07:19:10 +0000

    Tidy `t/math/month.t.js`.
    See #244.

--- t/math/month.t.js
+++ t/math/month.t.js
@@ -1,7 +1,8 @@
-#!/usr/bin/env node
-require("proof")(3, function (equal, tz, utc) {
+require('proof')(3, prove)
+
+function prove (assert) {
     var tz = require('timezone'), util = require('../util')
-  equal(tz(util.utc(2007, 2, 3), "+1 month"), util.utc(2007, 3, 3), "add month");
-  equal(tz(util.utc(2011, 10), "-1 month"), util.utc(2011, 9), "subtract month");
-  equal(tz(util.utc(2007, 11, 3), "+1 month"), util.utc(2008, 0, 3), "add month across year");
-});
+    assert(tz(util.utc(2007, 2, 3), '+1 month'), util.utc(2007, 3, 3), 'add month');
+    assert(tz(util.utc(2011, 10), '-1 month'), util.utc(2011, 9), 'subtract month');
+    assert(tz(util.utc(2007, 11, 3), '+1 month'), util.utc(2008, 0, 3), 'add month across year');
+}
