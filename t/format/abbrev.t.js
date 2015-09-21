e2f9bd4d34f4c1e622d35b4bcb77a9668ff69172
Alan Gutierrez <alan@prettyrobots.com>
2015-09-21 05:35:58 +0000

    Unset execute bit of format abbrev test.
    See #244.

--- t/format/abbrev.t.js
+++ t/format/abbrev.t.js
@@ -1,8 +1,7 @@
-#!/usr/bin/env node
-
 require('proof')(1, prove)
 
 function prove (assert) {
-    var util = require('../util'), tz = require('timezone')(require('timezone/America/Anchorage'))
+    var tz = require('timezone'), util = require('../util')
+    tz = tz(require('timezone/America/Anchorage'))
     assert(tz(util.utc(1946, 0, 1, 10), '%Z', 'America/Anchorage'), 'CAT', 'standard')
 }
