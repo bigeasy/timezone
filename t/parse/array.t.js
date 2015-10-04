4c2afcd4067e2a9630aabdd899b0576457350f65
Alan Gutierrez <alan@prettyrobots.com>
2015-10-04 03:33:26 +0000

    Tidy `parse` tests.
    See #244.

--- t/parse/array.t.js
+++ t/parse/array.t.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
-require("proof")(8, function (equal, tz, utc, moonwalk) {
+
+require("proof")(8, prove)
+
+function prove (equal) {
     var tz = require('timezone'), util = require('../util')
-  equal(tz([ 1980, 8, 18 ]), util.utc(1980, 7, 18), "year, month, date");
-  equal(tz([ 1969, 7, 21, 2, 56 ]), util.moonwalk, "moonwalk");
-  tz = tz(require("timezone/America/Detroit"));
-  equal(tz([ 1969, 7, 20, 21, 56 ], "America/Detroit"), util.moonwalk, "moonwalk in Detroit");
-  equal(typeof tz([ "X" ]), "function", "not an array date");
-  equal(tz([ 0, "Yoiks!" ]), 0, "thinks you want to flatten this");
-  equal(typeof tz([ 1970, 1, 1, 0, "X" ]), "function", "still not an array date");
-  equal(tz([ 0 ]), 0, "POSIX time");
-  equal(tz([ 1970, 1, 1 ]), 0, "year month");
-});
+    equal(tz([ 1980, 8, 18 ]), util.utc(1980, 7, 18), "year, month, date");
+    equal(tz([ 1969, 7, 21, 2, 56 ]), util.moonwalk, "moonwalk");
+    tz = tz(require("timezone/America/Detroit"));
+    equal(tz([ 1969, 7, 20, 21, 56 ], "America/Detroit"), util.moonwalk, "moonwalk in Detroit");
+    equal(typeof tz([ "X" ]), "function", "not an array date");
+    equal(tz([ 0, "Yoiks!" ]), 0, "thinks you want to flatten this");
+    equal(typeof tz([ 1970, 1, 1, 0, "X" ]), "function", "still not an array date");
+    equal(tz([ 0 ]), 0, "POSIX time");
+    equal(tz([ 1970, 1, 1 ]), 0, "year month");
+}
--- t/parse/numeric.t.js
+++ t/parse/numeric.t.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-require("proof")(3, function (equal, tz, utc, moonwalk) {
+
+require("proof")(3, prove)
+
+function prove (equal) {
     var tz = require('timezone')
-  equal(tz(7), 7, "integer");
-  equal(tz(new Date(7)), 7,  "Date");
-  equal(tz({ valueOf: function () { return 7 } }), 7, "valueOf");
-});
+    equal(tz(7), 7, "integer");
+    equal(tz(new Date(7)), 7,  "Date");
+    equal(tz({ valueOf: function () { return 7 } }), 7, "valueOf");
+}
