cfa41fef3301a5e740d71125a225e1813d7d1f41
Alan Gutierrez <alan@prettyrobots.com>
2015-10-06 01:24:10 -0500

    Fix parse test.
    See #244.

--- t/parse/array.t.js
+++ t/parse/array.t.js
@@ -5,7 +5,7 @@ require("proof")(8, prove)
 function prove (equal) {
     var tz = require('timezone'), util = require('../util')
     equal(tz([ 1980, 8, 18 ]), util.utc(1980, 7, 18), "year, month, date")
-    equal(tz([ 1969, 7, 21, 2, 56 ]), util.moonwalk, "moonwalk"
+    equal(tz([ 1969, 7, 21, 2, 56 ]), util.moonwalk, "moonwalk")
     tz = tz(require("timezone/America/Detroit"))
     equal(tz([ 1969, 7, 20, 21, 56 ], "America/Detroit"), util.moonwalk, "moonwalk in Detroit")
     equal(typeof tz([ "X" ]), "function", "not an array date")
