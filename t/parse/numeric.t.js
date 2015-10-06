1e6f7e89af2ceca6b5cc8346850fbf5050b37c0a
Alan Gutierrez <alan@prettyrobots.com>
2015-10-05 21:28:18 -0500

    Remove semi-colons from parse tests.

--- t/parse/numeric.t.js
+++ t/parse/numeric.t.js
@@ -4,7 +4,7 @@ require("proof")(3, prove)
 
 function prove (equal) {
     var tz = require('timezone')
-    equal(tz(7), 7, "integer");
-    equal(tz(new Date(7)), 7,  "Date");
-    equal(tz({ valueOf: function () { return 7 } }), 7, "valueOf");
+    equal(tz(7), 7, "integer")
+    equal(tz(new Date(7)), 7,  "Date")
+    equal(tz({ valueOf: function () { return 7 } }), 7, "valueOf")
 }
