2cd36e567dbc57a1f47ad3d33a5ce8f6ab247cd6
Alan Gutierrez <alan@prettyrobots.com>
2015-10-11 19:59:08 -0500

    Tidy version tests.
    See #244.

--- t/version/get.t.js
+++ t/version/get.t.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-require("proof")(1, function (equal, tz) {
+
+require('proof')(1, prove)
+
+function prove (assert) {
     var tz = require('timezone'), util = require('../util')
-  equal(tz(), "0.0.47", "get");
-});
+    assert(tz(), '0.0.47', 'get')
+}
