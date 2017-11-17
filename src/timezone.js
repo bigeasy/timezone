8834a45c47d6158868900219e366323fcbff8be5
Alan Gutierrez <alan@prettyrobots.com>
2017-11-17 15:53:07 +0000

    Release 1.0.9.
    Closes #283.

--- src/timezone.js
+++ src/timezone.js
@@ -109,7 +109,7 @@
   }
 
   function convert (vargs) {
-    if (!vargs.length) return "1.0.8";
+    if (!vargs.length) return "1.0.9";
 
     var request = Object.create(this)
       , adjustments = []
