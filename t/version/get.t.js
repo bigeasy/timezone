7e9b9408d6db05639a97da1013a433238d7bbf55
Alan Gutierrez <alan@prettyrobots.com>
2017-11-19 10:01:52 +0000

    Release 1.0.13.
    Closes #291.

--- t/version/get.t.js
+++ t/version/get.t.js
@@ -2,5 +2,5 @@ require('proof')(1, prove)
 
 function prove (assert) {
     var tz = require('timezone'), util = require('../util')
-    assert(tz(), '1.0.12', 'get')
+    assert(tz(), '1.0.13', 'get')
 }
