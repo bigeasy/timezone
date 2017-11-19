30c8dfb440cd1102acf6769f701b933ecbb0eb53
Alan Gutierrez <alan@prettyrobots.com>
2017-11-19 08:27:35 +0000

    Upgrade IANA TZ Database for `2017a`.
    Closes #286.

--- t/format/abbrev.t.js
+++ t/format/abbrev.t.js
@@ -3,5 +3,5 @@ require('proof')(1, prove)
 function prove (assert) {
     var tz = require('timezone'), util = require('../util')
     tz = tz(require('timezone/America/Anchorage'))
-    assert(tz(util.utc(1946, 0, 1, 10), '%Z', 'America/Anchorage'), 'CAT', 'standard')
+    assert(tz(util.utc(1946, 0, 1, 10), '%Z', 'America/Anchorage'), 'AST', 'standard')
 }
