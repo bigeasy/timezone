617f848bd1d13be319110a5e581430369f7f05ed
Ramazan SANCAR <ramazansancar4545@gmail.com>
2021-07-30 13:20:53 +0300

    Turkey Localization added.

---
+++ src/locales/tr_TR.js
@@ -0,0 +1,62 @@
+module.exports = {
+  "name": "tr_TR",
+  "day": {
+    "abbrev": [
+      "Paz",
+      "Pazt",
+      "Sal",
+      "Çarş",
+      "Perş",
+      "Cum",
+      "Cumt"
+    ],
+    "full": [
+      "Pazar",
+      "Pazartesi",
+      "Salı",
+      "Çarşamba",
+      "Perşembe",
+      "Cuma",
+      "Cumartesi"
+    ]
+  },
+  "month": {
+    "abbrev": [
+      "Oca",
+      "Şub",
+      "Mar",
+      "Nis",
+      "May",
+      "Haz",
+      "Tem",
+      "Ağu",
+      "Eyl",
+      "Eki",
+      "Kas",
+      "Ara"
+    ],
+    "full": [
+      "Ocak",
+      "Şubat",
+      "Mart",
+      "Nisan",
+      "Mayıs",
+      "Haziran",
+      "Temmuz",
+      "Ağustos",
+      "Eylül",
+      "Ekim",
+      "Kasım",
+      "Aralık"
+    ]
+  },
+  "meridiem": [
+    "AM",
+    "PM"
+  ],
+  "date": "%d/%m/%Y",
+  "time24": "%r",
+  "dateTime": "%a %d %b %Y %r %Z",
+  "time12": "%I:%M:%S %p",
+  "full": "%a %b %e %H:%M:%S %Z %Y"
+}
